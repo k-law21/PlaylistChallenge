c6f7e998294b1a647366176e7ac30bd66de3288e
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-10.local>
2019-10-26 19:50:56 -0400

    complete lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -2,13 +2,31 @@ package io.zipcoder;
 
 public class Music {
 
+
     private String[] playList;
 
     public Music(String[] playList){
         this.playList = playList;
     }
+        int counterForward = 0;
+        int counterBackward = 0;
 
-    public Integer selection(Integer startIndex, String selection){
-        return null;
+    public Integer selection(Integer startIndex, String selection) {
+
+
+        for (int i = 0; i <= startIndex; i++) {
+            counterForward++;
+
+        }
+        for (int j = 0; j <= playList.length; j++){
+            counterBackward++;
+    }
+        if(counterForward < counterBackward){
+            return counterForward;
+        }
+        else{
+            return counterBackward;
+        }
+
     }
 }
